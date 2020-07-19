a537ca73e8bd8f3011671673984c1064d00bfbdf
sav4yk <mail@sav4yk.ru>
2020-07-19 16:48:39 +0300

    Parse data from news sites

---
+++ models/News.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "news".
+ *
+ * @property int $id
+ * @property string $channel
+ * @property string $title
+ * @property string $link
+ * @property int $pubDate
+ * @property string $category
+ * @property string $description
+ */
+class News extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'news';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['channel', 'title', 'link', 'pubDate', 'category', 'description'], 'required'],
+            [['pubDate'], 'integer'],
+            [['description','title'], 'string'],
+            [['channel', 'link', 'category'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'channel' => 'Channel',
+            'title' => 'Title',
+            'link' => 'Link',
+            'pubDate' => 'Pub Date',
+            'category' => 'Category',
+            'description' => 'Description',
+        ];
+    }
+}
